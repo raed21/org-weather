f89ca242725606a9d787356a95a765fca3cd26c8
raed21 <raed.m.iskandar@gmail.com>
2021-08-14 11:24:34 -0400

    Custom mod of the weather output

--- org-weather.el
+++ org-weather.el
@@ -39,13 +39,13 @@
 (defvar org-weather-location "Graz,AT")
 
 ;; Define the format string to display in the agenda, see below for available wildcards
-(defvar org-weather-format "Weather: %desc, %tmin-%tmax%tu, %p%pu, %h%hu, %s%su")
+(defvar org-weather-format "Weather: %2.2f%tu - ");%desc, %tmin-%tmax%tu, %p%pu, %h%hu, %s%su")
 
 ;; Your openweather API key.
 (defvar org-weather-api-key  "")
 
 ;; The api url, no need to change ususally
-(defvar org-weather-api-url "api.openweathermap.org/data/2.5/weather?q=%s&mode=json&units=metri&appid=%s")
+(defvar org-weather-api-url "http://api.openweathermap.org/data/2.5/weather?q=%s&mode=json&units=metric&APPID=%s")
 
 (defvar org-weather-api-timeout 2)
 
@@ -74,49 +74,58 @@
          (cache-key (format-time-string "%F" (seconds-to-time timestr)))
 
          ;; extract fields from the 'weather' element
-         (weather (cdr (assoc 'weather item)))
-         (main (org-weather-weather weather 'main))
-         (desc (org-weather-weather weather 'description))
-         (icon (org-weather-weather weather 'icon))
+         ;;(weather (cdr (assoc 'id (assoc 'weather item))))
+         ;;(main (org-weather-weather weather 'main))
+         ;;(desc (org-weather-weather weather 'description))
+         ;;(icon (org-weather-weather weather 'icon))
 
          ;; extract fields from the 'temp' element
-         (temperature (cdr (assoc 'temp item)))
-         (tmin (number-to-string (round (org-weather-temp temperature 'min))))
-         (tmax (number-to-string (round (org-weather-temp temperature 'max))))
-         (tmorn (number-to-string (round (org-weather-temp temperature 'morn))))
-         (tday (number-to-string (round (org-weather-temp temperature 'day))))
-         (teve (number-to-string (round (org-weather-temp temperature 'eve))))
-         (tnight (number-to-string (round (org-weather-temp temperature 'night))))
+         (main (cdr (assoc 'main item)))
+         (temperature (number-to-string (cdr (assoc 'temp main))))
+         ;;(tmin (number-to-string (cdr (assoc 'temp_min main)))
+         ;;(tmax (number-to-string (cdr (assoc 'temp_max main)))
+         ;;(feels_like (number-to-string (cdr (assoc 'feels_like main)))
+         ;;(presure (number-to-string (cdr (assoc 'presure main)))
+         ;;(humidity (number-to-string (cdr (assoc 'humidity main)))
+         ;;(tmin (number-to-string (round (org-weather-temp temperature 'min))))
+         ;;(tmax (number-to-string (round (org-weather-temp temperature 'max))))
+         ;;(tmorn (number-to-string (round (org-weather-temp temperature 'morn))))
+         ;;(tday (number-to-string (round (org-weather-temp temperature 'day))))
+         ;;(teve (number-to-string (round (org-weather-temp temperature 'eve))))
+         ;;(tnight (number-to-string (round (org-weather-temp temperature 'night))))
 
          ;; other fields
-         (pressure (number-to-string (round (cdr (assoc 'pressure item)))))
-         (humidity (number-to-string (round (cdr (assoc 'humidity item)))))
-         (speed (number-to-string (round (cdr (assoc 'speed item)))))
+         ;;(pressure (number-to-string (round (cdr (assoc 'pressure item)))))
+         ;;(humidity (number-to-string (round (cdr (assoc 'humidity item)))))
+         ;;(speed (number-to-string (round (cdr (assoc 'speed item)))))
 
          ;; replace in org-weather-format
          (agenda-str org-weather-format)
 
-         (agenda-str (org-weather-replace "%main" main agenda-str))
-         (agenda-str (org-weather-replace "%desc" desc agenda-str))
-         (agenda-str (org-weather-replace "%icon" icon agenda-str))
+         ;;(agenda-str (org-weather-replace "%main" main agenda-str))
+         ;;(agenda-str (org-weather-replace "%desc" desc agenda-str))
+         ;;(agenda-str (org-weather-replace "%icon" icon agenda-str))
 
+         (agenda-str (org-weather-replace "%2.2f" temperature agenda-str))
          (agenda-str (org-weather-replace "%tu" org-weather-temperature-unit agenda-str))
-         (agenda-str (org-weather-replace "%tmin" tmin agenda-str))
-         (agenda-str (org-weather-replace "%tmax" tmax agenda-str))
-         (agenda-str (org-weather-replace "%tmorn" tmorn agenda-str))
-         (agenda-str (org-weather-replace "%tday" tday agenda-str))
-         (agenda-str (org-weather-replace "%teve" teve agenda-str))
-         (agenda-str (org-weather-replace "%tnight" tnight agenda-str))
+          ;;(agenda-str (org-weather-replace "%tmin" tmin agenda-str))
+          ;;(agenda-str (org-weather-replace "%tmax" tmax agenda-str))
+         ;;(agenda-str (org-weather-replace "%tmorn" tmorn agenda-str))
+         ;;(agenda-str (org-weather-replace "%tday" tday agenda-str))
+         ;;(agenda-str (org-weather-replace "%teve" teve agenda-str))
+         ;;(agenda-str (org-weather-replace "%tnight" tnight agenda-str))
 
-         (agenda-str (org-weather-replace "%pu" org-weather-pressure-unit agenda-str))
-         (agenda-str (org-weather-replace "%p" pressure agenda-str))
-         (agenda-str (org-weather-replace "%hu" org-weather-humidity-unit agenda-str))
-         (agenda-str (org-weather-replace "%h" humidity agenda-str))
-         (agenda-str (org-weather-replace "%su" org-weather-speed-unit agenda-str))
-         (agenda-str (org-weather-replace "%s" speed agenda-str))
+          ;;(agenda-str (org-weather-replace "%pu" org-weather-pressure-unit agenda-str))
+          ;;(agenda-str (org-weather-replace "%p" pressure agenda-str))
+          ;;(agenda-str (org-weather-replace "%hu" org-weather-humidity-unit agenda-str))
+          ;;(agenda-str (org-weather-replace "%h" humidity agenda-str))
+          ;;(agenda-str (org-weather-replace "%su" org-weather-speed-unit agenda-str))
+         ;;(agenda-str (org-weather-replace "%s" speed agenda-str))
 
          )
-    (puthash cache-key agenda-str org-weather-data)))
+    ;;(message "temp: %2.2f" temp)
+    (puthash cache-key agenda-str org-weather-data)
+    ))
 
 (defun org-weather-replace (what with in)
   (replace-regexp-in-string (regexp-quote what) with in))
@@ -133,7 +142,7 @@
   "Refreshes the weather data"
   (interactive)
   (setq org-weather-data (make-hash-table :test 'equal))
-  (mapcar 'org-weather-add-to-cache (cdr (assoc 'list (org-weather-load))))
+  (org-weather-add-to-cache (cdr (cdr (cdr (org-weather-load))))) ;;(assoc 'list
   (setq org-weather-initialized t))
 
 (defun org-weather-string (&optional date)
